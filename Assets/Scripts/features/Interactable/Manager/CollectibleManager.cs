566e6b927db2ae6dd8c40b09b84c75c7283781af
Diaz Abdul <67885615+diazabdul@users.noreply.github.com>
2025-12-16 18:42:27 +0700

    Add debugging event and update collectible logic
    Introduced Debugging_Event script for testing and debugging purposes. Updated CollectibleManager to invoke a new event when a specific number of collectibles is reached, and added related UnityEvent fields. Modified the In Game scene to include the new Collectible Manager GameObject and adjusted some object properties. Also added a new scene to the build settings.

--- Assets/Scripts/features/Interactable/Manager/CollectibleManager.cs
+++ Assets/Scripts/features/Interactable/Manager/CollectibleManager.cs
@@ -8,6 +8,10 @@ public class CollectibleManager : MonoBehaviour
 
   public UnityEvent firstPuzzlePickup;
 
+    [Header("Event")]
+    [SerializeField] int numberCollectible = 2;
+    public UnityEvent callInSpesificCollectible;
+
   private void OnEnable()
   {
     EventBus.Subscribe<CollectibleType>(ListenerCollectible);
@@ -18,27 +22,34 @@ public class CollectibleManager : MonoBehaviour
 
   }
 
-  void ListenerCollectible(CollectibleType type)
-  {
-    switch (type)
-    {
-      case CollectibleType.Key:
-        counting.IncrementKey();
-        break;
-      case CollectibleType.Puzzle:
-        // Check if first puzzle pickup BEFORE incrementing
-        if (counting.GetPuzzleCount() == 0)
-        {
-          firstPuzzlePickup?.Invoke();
-          // Publish event for other systems (like SanityTimerSystem)
-          EventBus.Publish(new FirstPuzzleCollectedEvent());
+      void ListenerCollectible(CollectibleType type)
+      {
+            switch (type)
+            {
+              case CollectibleType.Key:
+                counting.IncrementKey();
+                break;
+              case CollectibleType.Puzzle:
+                // Check if first puzzle pickup BEFORE incrementing
+                if (counting.GetPuzzleCount() == 0)
+                {
+                  firstPuzzlePickup?.Invoke();
+                  // Publish event for other systems (like SanityTimerSystem)
+                  EventBus.Publish(new FirstPuzzleCollectedEvent());
+                }        
+                counting.IncrementPuzzle();
+
+                if(numberCollectible == counting.GetPuzzleCount())
+                {
+                    callInSpesificCollectible?.Invoke();
+                    Debug.Log("Open Door Again");
+                }
+                break;
+
+                default:
+                break;
         }
-        counting.IncrementPuzzle();
-        break;
-      default:
-        break;
-    }
-  }
+      }
 
   /// <summary>
   /// Event published when the first puzzle piece is collected
